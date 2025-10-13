5b011fc831030837c3035d4499c39795d3baef13
Success-1 <itumasuccess29@gmail.com>
2025-10-13 11:51:39 +0200

    Created the project pseudocode for a better and an organized workflow

---
+++ copilot_shell_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Prompt user(student) to enter the name of the assignment to be checked for submission
+
+#Write the assignment name in submission_reminder_name/config/config.env
+
+#Replace the current assignment name with the new name entered by the user
+
+#Run the startup.sh to check the non-submission status of the new assignment that is saved in config/config.env
+
+#Test and push
---
+++ create_environment.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Creating assignment reminder app. This script creates the directory structure.
+
+#The pseudocode/steps:
+
+#Prompt user(student) to enter their name 
+
+#Create directory(submission_reminder_nameentered) for every user that enters his/her name
+
+#Create subdirectory 1 called (app) and create a file called (reminder.sh) inside it.
+
+#Create subdirectory 2 called (modules) and create a file called (functions.sh) inside it.
+
+#Create subdirectory 3 called (assets) and create a file called (submission.txt) in it.
+
+#Create subdirectory 4 called (config) and create a file called (config.env) in it.
+
+#Create a file called (startup.sh) directory inside the "submission_reminder_nameentered"
+
+#Copy the code from the attached files and paste in the files created.
+
+#Make this "submission_reminder_nameentered" and all of its content automatic created for every user who enters their name.
+
+#Make the startup.sh file executable
+
+#And startup makes all other files with .sh extension executable
+
+#Add at least 5 users for better functionality testing.
+
+#Over to the copilot_shell_script.sh to continue....
